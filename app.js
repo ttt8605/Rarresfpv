e7273db8b0bc2808f29f5bf017815852b91aabf5
Theo <theodorapostoaie@gmail.com>
2024-04-10 12:03:32 +0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+const ejsMate = require('ejs-mate');
+//connecting the ejs files
+const path = require('path');
+
+app.engine('ejs',ejsMate)
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 app.use('/homepage',(req,res)=>{
-    res.send('/pages/index.html')
+    res.render('/index')
 })
 
 
